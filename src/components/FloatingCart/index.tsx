c57135964789a5971552a7420ffdae7d51b391da
taitson <66077252+taitson@users.noreply.github.com>
2021-10-03 15:10:51 -0300

    Initial commit

---
+++ src/components/FloatingCart/index.tsx
@@ -0,0 +1,63 @@
+import React, { useState, useMemo } from 'react';
+
+import { useNavigation } from '@react-navigation/native';
+
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import {
+  Container,
+  CartPricing,
+  CartButton,
+  CartButtonText,
+  CartTotalPrice,
+} from './styles';
+
+import formatValue from '../../utils/formatValue';
+
+import { useCart } from '../../hooks/cart';
+
+// Calculo do total
+// Navegação no clique do TouchableHighlight
+
+const FloatingCart: React.FC = () => {
+  const { products } = useCart();
+
+  const navigation = useNavigation();
+
+  const cartTotal = useMemo(() => {
+    //accumulator = onde vamos acumular todos os retornos
+    //product é cada produto no carrinho
+    const total = products.reduce((accumulator, product) => {
+      const subtotal = product.price * product.quantity;
+
+      return accumulator + subtotal;
+    }, 0); //valor inicial do reduce
+    return formatValue(total);
+  }, [products]);
+
+  const totalItensInCart = useMemo(() => {
+    const totalItems = products.reduce((accumulator, product) => {
+
+      return accumulator + product.quantity;
+    }, 0); //valor inicial do reduce
+    return totalItems;
+  }, [products]);
+
+
+  return (
+    <Container>
+      <CartButton
+        testID="navigate-to-cart-button"
+        onPress={() => navigation.navigate('Cart')}
+      >
+        <FeatherIcon name="shopping-cart" size={24} color="#fff" />
+        <CartButtonText>{`${totalItensInCart} itens`}</CartButtonText>
+      </CartButton>
+
+      <CartPricing>
+        <CartTotalPrice>{cartTotal}</CartTotalPrice>
+      </CartPricing>
+    </Container>
+  );
+};
+
+export default FloatingCart;
---
+++ src/hooks/cart.tsx
@@ -0,0 +1,111 @@
+import React, {
+  createContext,
+  useState,
+  useCallback,
+  useContext,
+  useEffect,
+} from 'react';
+
+import AsyncStorage from '@react-native-community/async-storage';
+
+interface Product {
+  id: string;
+  title: string;
+  image_url: string;
+  price: number;
+  quantity: number;
+}
+
+interface CartContext {
+  products: Product[];
+  addToCart(item: Omit<Product, 'quantity'>): void;
+  increment(id: string): void;
+  decrement(id: string): void;
+}
+
+const CartContext = createContext<CartContext | null>(null);
+
+const CartProvider: React.FC = ({ children }) => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    async function loadProducts(): Promise<void> {
+      const storagedProducts = await AsyncStorage.getItem(
+        '@GoMarketPlace:products'
+      );
+
+      if (storagedProducts) {
+        setProducts([...JSON.parse(storagedProducts)]);
+      }
+    }
+
+    loadProducts();
+  }, [products]);
+
+  const addToCart = useCallback(async product => {
+    const cartHasProduct = products.find(p => p.id === product.id);
+    if (cartHasProduct) {
+      setProducts(
+        products.map(p =>
+          p.id === product.id ?
+            { ...product, quantity: p.quantity + 1 } : p
+        ),
+      );
+    } else {
+      setProducts([...products, { ...product, quantity: 1 }]);
+    }
+    await AsyncStorage.setItem(
+      '@GoMarketPlace:products',
+      JSON.stringify(products),
+    );
+  }, [products]);
+
+  const increment = useCallback(async id => {
+
+    const addProduct = products.map(p =>
+      p.id === id ? { ...p, quantity: p.quantity + 1 } : p,
+    );
+
+    setProducts(addProduct);
+
+    await AsyncStorage.setItem(
+      '@GoMarketPlace:products',
+      JSON.stringify(addProduct),
+    );
+
+  }, [products]);
+
+  const decrement = useCallback(async id => {
+
+    const selectedProduct = products.map(p =>
+      p.id === id ? { ...p, quantity: p.quantity - 1 } : p
+    );
+
+    setProducts(selectedProduct);
+
+    await AsyncStorage.setItem(
+      '@GoMarketPlace:products',
+      JSON.stringify(selectedProduct)
+    );
+
+  }, [products]);
+
+  const value = React.useMemo(
+    () => ({ addToCart, increment, decrement, products }),
+    [products, addToCart, increment, decrement],
+  );
+
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
+
+function useCart(): CartContext {
+  const context = useContext(CartContext);
+
+  if (!context) {
+    throw new Error(`useCart must be used within a CartProvider`);
+  }
+
+  return context;
+}
+
+export { CartProvider, useCart };
